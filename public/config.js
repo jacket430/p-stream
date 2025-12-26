ed71218be44edd946375222d9ae7aa72c75e36f2
Avery <34522242+jacket430@users.noreply.github.com>
2025-12-25 22:26:29 -0700

    remove example.env and rebrand to 430Stream

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "your_tmdb_api_key_here",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
